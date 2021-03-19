ffb7f6a7a1a9edf9aef2dcd5fb416207a2b6548e
Thiago Robles <roblesthiago38@gmail.com>
2021-03-18 23:38:08 -0300

    start home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
-import { Container } from "@chakra-ui/react";
+import { ArrowDownIcon } from "@chakra-ui/icons";
+import {
+  Background,
+  DownArrow,
+  Presentation,
+  Text,
+  Container,
+} from "../styles/Home";
 
 const App: React.FC = () => {
   return (
-    <Container maxW="min-content" centerContent>
-      Oiew
+    <Container height="100vh">
+      <Background />
+      <DownArrow>
+        <ArrowDownIcon
+          bgSize="contain"
+          boxSize="24"
+          color="red.900"
+          style={{ zIndex: 999 }}
+        />
+      </DownArrow>
+      <Presentation>
+        <Text>Hi, i'm Thiago Robles!</Text>
+      </Presentation>
     </Container>
   );
 };
--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -1,5 +1,41 @@
 import styled from "styled-components";
+import {
+  Text as ChakraText,
+  TextProps,
+  Container as ChakraContainer,
+  ContainerProps,
+} from "@chakra-ui/react";
+import Image from "next/image";
 
-export const Container = styled.div`
-  max-width: 1000px;
+import BackgroundImage from "../assets/images/background.jpg";
+
+export const Container = styled(ChakraContainer)<ContainerProps>``;
+
+export const Background = styled(Image).attrs({
+  src: BackgroundImage,
+  layout: "fill",
+})`
+  width: 100vw;
+  height: 100vw;
+  display: flex;
+  flex: 1;
+`;
+
+export const DownArrow = styled.span`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+`;
+
+export const Presentation = styled(ChakraContainer)<ContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: end;
+
+  flex: 1;
+  z-index: 999;
+`;
+
+export const Text = styled(ChakraText)<TextProps>`
+  z-index: 999;
 `;
